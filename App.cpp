05b0e92cf8d38ae0c0430797b90571fe725e23b4
Keagon King <keagonk@gmail.com>
2024-07-23 10:14:35 -0500

    1

--- App.cpp
+++ App.cpp
@@ -3,8 +3,23 @@
 
 #include <iostream>
 
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\n";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
 
 int main() {
 	// glfw: initialize and configure
@@ -39,6 +54,121 @@ int main() {
 		return -1;
 	}
 
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+
+	// Create and compile vertex shader at runtime
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Create and compile fragment shader at runtime
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	/* Shader program
+	*	To use our recently compiled shaders, we have to link them to a
+	*	shader program object and then activate this shader program when
+	*	rendering objects. The activated shader program's shaders will be
+	*	used when we issue render calls.
+	*/
+	unsigned int shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+
+	float vertices[] = {
+		0.5f, 0.5f, 0.0f,	// top right
+		0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f,	// bottom left
+		-0.5f, 0.5f, 0.0f	// top left
+	};
+
+	unsigned int indices[] = {	// note that we start from 0
+		0,1,3,					// first triangle
+		1,2,3					// second triangle
+	};
+
+	/* A vertex buffer object(VBO) can store a large number of vertices in
+	*  the GPU's memory. We use this to send large batches of data to the GPU
+	*  all at once, since CPU->GPU data transfers are slow, we want to send as
+	*  much as we can at a time. Once at the GPU, the vertex shader has nearly
+	*  instant access to the vertices.
+	*/
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+	glGenBuffers(1, &VBO);
+
+	
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s),
+	// and then configure vertex attribute(s)
+	glBindVertexArray(VAO);
+	
+	/* OpenGL has many types of buffer objects and the buffer type of a vertex
+	// buffer object is GL_ARRAY_BUFFER. OpenGL allows us to bind to several buffers
+	// at once as long as they have a different buffer type.
+	*/
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	/* From this point on, any buffer calls we make on the GL_ARRAY_BUFFER
+	*  target will be used to configure the currently bound buffer, which is VBO.
+	*  We can then make a call to the glBufferData function that copies the 
+	*  previously defined vertex data into the buffer's memory.
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	/* Linking vertex attributes
+	*	The vertex shader allows us to specify any input we want in the form
+	*	of vertex attributes, and while this allows for great flexibility, it 
+	*	does mean we have to manually specify what part of our input data goes
+	*	to which vertex attribute in the vertex shader. This means we have to 
+	*	specify how OpenGL should interpret the vertex data before rendering.
+	*/
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window)) {
@@ -48,14 +178,32 @@ int main() {
 
 		// render
 		// ------
+		
+		// wireframe
+		// ---------
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw our first triangle
+		// -----------------------
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved, etc.)
 		// -------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 
 	// glfw: terminate, clearing all previously allocated GLFW resources
 	// -----------------------------------------------------------------
@@ -76,3 +224,4 @@ void processInput(GLFWwindow* window) {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, 800, 600);
 }
+
